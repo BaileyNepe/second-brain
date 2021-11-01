f2750a891e0774d199fd154579fd6620a8222c25
Hayley Crouse <hayleycrouse@developersinstitute.co.nz>
2021-11-02 12:10:37 +1300

    create todo list

--- backend/server.js
+++ backend/server.js
@@ -9,4 +9,22 @@ appServer.get("/", (req, res) => {
   res.send("Api is running ...");
 });
 
+// API Routes
+
+appServer.get("/api/todo", (req, res) => {});
+
+// TODO: Create dummy database
+
+// TODO: Create get route to get todo items from database
+
+// TODO: Create a post route to add to item database
+
+// TODO: Create a update route to update item in database
+
+// TODO: Create a delete route to delete item in database
+
+// TODO: connect real database (mongoDB)
+
+// TODO: Separate routes, models, controllers into separate folders/files
+
 appServer.listen(PORT, console.log(`Server is listening on port ${PORT}`));
