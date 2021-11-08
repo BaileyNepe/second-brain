0ca0339a36fe55ce6779883492075be1a89b65d6
Rhonda Crouse <rhondacrouse@developersinstitute.co.nz>
2021-11-09 11:06:23 +1300

    have added grid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,13 +10,19 @@ const App = () => {
   return (
     <>
       <Router>
+        <div className="app">
         <Header />
+        <div className="body">
         <Routes>
+
           <Route path="/" element={<HomeScreen />} />
           <Route path="/todo" element={<TodoScreen />} />
         </Routes>
+        </div>
         <Footer />
+        </div>
       </Router>
+      
     </>
   );
 };
@@ -24,7 +30,6 @@ const App = () => {
 export default App;
 
 // TODO make grid for page
-// TODO add in nav bar into header
 // TODO hamburger menu for mobile screen with fun animations - slide in from side
 // Create homescreen - display list of todos and input and add
 // Create component for CRUD
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,20 @@
-import React from "react";
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/todo">Todo</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
