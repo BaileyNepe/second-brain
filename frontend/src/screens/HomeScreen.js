03a0a76fefaadb9c3d885843b448d03035a385bf
Rhonda Crouse <rhondacrouse@developersinstitute.co.nz>
2021-11-12 11:31:27 +1300

    usestates put in

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,40 @@
+import { useEffect, useState } from 'react';
+
 const HomeScreen = () => {
-  const dummy = [
-    { title: 'hello', isComplete: false },
-    { title: 'test', isComplete: false },
-  ];
+  const [todos, setTodos] = useState([]);
+  const fetchTodos = async () => {
+    try {
+      const todos = await fetch("http://localhost:5000/api/todos");
+      setTodos(todos);
+    } catch (e) {
+      console.error('Database query failed');
+      return null;
+    }
+  };
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  console.log(todos)
   return (
     <div>
-      <div>
-        {dummy.map((todoItem) => (
-          <div>
-            <div>{todoItem.title}</div>
-            <div></div>
-          </div>
-        ))}
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Edit</th>
+            <th>Todo</th>
+            <th>Done</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todoItem, index ) => (
+            <tr key={index}>
+              <td> edit</td>
+              <td>{todoItem.title}</td>
+              <td> description</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
       <button className="button">+</button>
     </div>
   );
